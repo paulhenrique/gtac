faff908e14da3fb1e720180dcaac55052e8da65a
Paulo Henrique <phvcandido@gmail.com>
2021-05-15 16:34:08 -0300

    Completando estilização do header com button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,20 @@ import './style.scss';
 function Header() {
   return (
     <header id="header">
-      <div className="containerImage">
-        <img src={LogoIFSP} alt="Logo IFSP" />
+      <div className="containerWelcome">
+        <div className="containerImage">
+          <img src={LogoIFSP} alt="Logo IFSP" />
+        </div>
+        <div className="containerText">
+          <h1>GTAC</h1>
+          <p>Grupo de Tecnologias e Aplicações da Computação</p>
+
+        </div>
       </div>
-      <div className="containerText">
-        <h1>GTAC</h1>
-        <p>Grupo de Tecnologias e Aplicações da Computação</p>
+      <div className="containerButton">
+        <button>
+          <i className="fas fa-arrow-down"></i>
+        </button>
       </div>
     </header>
   )
