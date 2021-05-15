697aab3109b6dd676c801421ec27d214dd813942
Paulo Henrique <phvcandido@gmail.com>
2021-05-15 17:14:18 -0300

    Completando estilo dos projetos

---
+++ src/components/CardProject/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Pen from '../../assets/img/pen.png';
+import './style.scss';
+
+interface ProjectPropp {
+  title: string;
+  description: string;
+  href: string;
+}
+
+interface ProjectProps {
+  project: ProjectPropp;
+}
+
+const Project: React.FC<ProjectProps> = ({ project }) => {
+  return (
+    <a href="#" className="projectCard">
+      <div >
+        <h1>{project.title}</h1>
+        <p>{project.description}</p>
+      </div>
+      <img src={Pen} alt="Pen" />
+    </a>
+  )
+}
+
+export default Project
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Project from '../CardProject'
 import SectionTitle from '../SectionTitle'
+import './style.scss';
 
 function Projects() {
+  const project = {
+    title: 'ElectrosFI',
+    description: 'Eletromagnetic Simulator of The Federal Institute of São Paulo',
+    href: 'project/aaaa'
+  }
   return (
-    <div>
+    <div className="ProjectsSection">
       <SectionTitle text="Projects" />
+      <div className="containerProjects">
+        {
+          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(e => (
+            <Project project={project} key={e} />
+          ))
+        }
+      </div>
     </div>
   )
 }
