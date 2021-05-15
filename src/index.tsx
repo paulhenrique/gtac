21091c8f315396c9c062abfd4b2235f793905885
Paulo Henrique <phvcandido@gmail.com>
2021-05-15 15:50:45 -0300

    Organizando ambiente

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Router from './Router';
 
 ReactDOM.render(
-  <App />,
+  <Router />,
   document.getElementById('root')
 );
