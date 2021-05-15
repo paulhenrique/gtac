6cd6b29bad90c4b3351823edb84d2194304875b2
Paulo Henrique <phvcandido@gmail.com>
2021-05-15 19:36:12 -0300

    Atualizando projetos

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,18 +4,70 @@ import SectionTitle from '../SectionTitle'
 import './style.scss';
 
 function Projects() {
-  const project = {
-    title: 'ElectrosFI',
-    description: 'Eletromagnetic Simulator of The Federal Institute of São Paulo',
-    href: 'project/aaaa'
-  }
+  const projects = [
+    {
+      title: 'ElectrosFI',
+      description: 'Eletromagnetic Simulator of The Federal Institute of São Paulo',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'Agriloggi',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'Panda',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'GPS',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'Hello Yotta',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'GLMT Scattering',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'Fábrica de Software',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'SGE',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'Show da Física',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'ExpWeb',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    },
+    {
+      title: 'SVSA',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa laborum asperiores ad quam? Consectetur architecto inventore',
+      href: 'https://itp.ifsp.edu.br'
+    }
+  ]
   return (
     <div className="ProjectsSection" id="projects">
       <SectionTitle text="Projects" />
       <div className="containerProjects">
         {
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(e => (
-            <Project project={project} key={e} />
+          projects.map(project => (
+            <Project project={project} key={project.title} />
           ))
         }
       </div>
